6c49def6cb41199c18a992caadacc0cbc31278db
mekdinside <mericynthia00@gmail.com>
2024-04-23 20:37:18 +0700

    Initial commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->tinyInteger('type')->default(0);
+            /* Users: 0=>User, 1=>Superadmin, 2=>Admin, 3=>Dosen, 4=>Mahasiswa, 5=>Tendik, 6=>Akademik, 7=>Keuangan, 8=>Direktur, 9=>Wakildekan1, 10=>Wakildekan2, 11=>Wakildekan3, 12=>AdminLppm, 13=>AdminSdm */
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,174 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Auth::routes();
+/*------------------------------------------
+--------------------------------------------
+All Normal Users Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:user'])->group(function () {
+    Route::get('/home', [
+        HomeController::class, 
+        'index'
+        ])->name('home');
+});
+/*------------------------------------------
+--------------------------------------------
+All Admin Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:admin'])->group(function () {
+    Route::get('/admin/home', [
+        HomeController::class,
+        'adminHome'
+        ])->name('admin.home');
+});
+/*------------------------------------------
+--------------------------------------------
+All SuperAdmin Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:superadmin'])->group(function () {
+    Route::get('/superadmin/home', [
+        HomeController::class,
+        'superadminHome'
+        ])->name('superadmin.home');
+});
+/*------------------------------------------
+--------------------------------------------
+All Dosen Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:dosen'])->group(function () {
+    Route::get('/dosen/home', [
+        HomeController::class,
+        'dosenHome'
+        ])->name('dosen.home');
+});
+/*------------------------------------------
+--------------------------------------------
+All Mahasiswa Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:mahasiswa'])->group(function () {
+    Route::get('/mahasiswa/home', [
+        HomeController::class,
+        'mahasiswaHome'
+        ])->name('mahasiswa.home');
+});
+/*------------------------------------------
+--------------------------------------------
+All Tendik Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:tendik'])->group(function () {
+    Route::get('/tendik/home', [
+        HomeController::class,
+        'tendikHome'
+        ])->name('tendik.home');
+});
+/*------------------------------------------
+--------------------------------------------
+All Admin Akademik Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:akademik'])->group(function () {
+    Route::get('/akademik/home', [
+        HomeController::class,
+        'akademikHome'
+        ])->name('akademik.home');
+});
+/*------------------------------------------
+--------------------------------------------
+All Admin Keuangan Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:keuangan'])->group(function () {
+    Route::get('/keuangan/home', [
+        HomeController::class,
+        'keuanganHome'
+        ])->name('keuangan.home');
+});
+/*------------------------------------------
+--------------------------------------------
+All Direktur Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:direktur'])->group(function () {
+    Route::get('/direktur/home', [
+        HomeController::class,
+        'direkturHome'
+        ])->name('direktur.home');
+});
+/*------------------------------------------
+--------------------------------------------
+All wd1 Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:wd1'])->group(function () {
+    Route::get('/wd1/home', [
+        HomeController::class,
+        'wd1Home'
+        ])->name('wd1.home');
+});
+/*------------------------------------------
+--------------------------------------------
+All wd2 Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:wd2'])->group(function () {
+    Route::get('/wd2/home', [
+        HomeController::class,
+        'wd2Home'
+        ])->name('wd2.home');
+});
+/*------------------------------------------
+--------------------------------------------
+All wd3 Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:wd3'])->group(function () {
+    Route::get('/wd3/home', [
+        HomeController::class,
+        'wd3Home'
+        ])->name('wd3.home');
+});
+/*------------------------------------------
+--------------------------------------------
+All lppm Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:lppm'])->group(function () {
+    Route::get('/lppm/home', [
+        HomeController::class,
+        'lppmHome'
+        ])->name('lppm.home');
+});
+/*------------------------------------------
+--------------------------------------------
+All sdm Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:sdm'])->group(function () {
+    Route::get('/sdm/home', [
+        HomeController::class,
+        'sdmHome'
+        ])->name('sdm.home');
+});
